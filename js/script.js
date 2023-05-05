1dce6a81edb0cc0f1e78d6b6f38b15a796ef038f
Joseph Sanchez <josep@DESKTOP-EOTHT6C.localdomain>
2023-05-05 01:06:53 -0400

    added script.js

---
+++ js/script.js
@@ -0,0 +1,46 @@
+function goToLink(link){
+ 
+    console.log(link.value);
+    window.open(link.value);
+};
+
+var addZoom = target => {
+    // (A) get container and image source
+    let container = document.getElementById(target),
+        imgsrc = container.currentStyle || window.getComputedStyle(container, false);
+        imgsrc = imgsrc.backgroundImage.slice(4, -1).replace(/"/g, "");
+   
+    // (B) LOAD IMAGE + ATTACH ZOOM
+    let img = new Image();
+    img.src = imgsrc;
+    img.onload = () => {
+      // (B1) CALCULATE ZOOM RATIO
+      let ratio = img.naturalHeight / img.naturalWidth,
+          percentage = ratio * 100 + "%";
+   
+      // (B2) ATTACH ZOOM ON MOUSE MOVE
+      container.onmousemove = e => {
+        let rect = e.target.getBoundingClientRect(),
+            xPos = e.clientX - rect.left,
+            yPos = e.clientY - rect.top,
+            xPercent = xPos / (container.clientWidth / 80) + "%",
+            yPercent = yPos / ((container.clientWidth * ratio) / 100) + "%";
+   
+        Object.assign(container.style, {
+          backgroundPosition: xPercent + " " + yPercent,
+          backgroundSize: img.naturalWidth + "px"
+        });
+      };
+   
+      // (B3) RESET ZOOM ON MOUSE LEAVE
+      container.onmouseleave = e => {
+        Object.assign(container.style, {
+          backgroundPosition: "center",
+          backgroundSize: "cover"
+        });
+      };
+    }
+  };
+   
+  // (C) ATTACH FOLLOW ZOOM
+  window.onload = () => addZoom("zoomC");
